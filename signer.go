1bfd270360ac78569eec280e279440d519a59771
Roman Larionov <larionovra@gmail.com>
2024-08-27 22:57:39 +0300

    Fix

--- signer.go
+++ signer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
 )
@@ -23,33 +24,33 @@ func ExecutePipeline(jobs ...job) {
 	}
 }
 
-func parallelProcessCrc32(data string, item chan string) {
-	item <- DataSignerCrc32(data)
-	close(item)
-}
-
 func SingleHash(in, out chan interface{}) {
 	wg := &sync.WaitGroup{}
 	md5Mux := &sync.Mutex{}
 
 	for val := range in {
-		var chans []chan string
-		var i int
 		wg.Add(1)
 		go func(val interface{}) {
 			defer wg.Done()
-			chans = append(chans, make(chan string, 1)) // crc32(data)
-			go parallelProcessCrc32(fmt.Sprint(val), chans[i])
-			md5Mux.Lock()
-			md5 := DataSignerMd5(fmt.Sprint(val)) // md5(data)
-			md5Mux.Unlock()
-			chans = append(chans, make(chan string, 1))
-			go parallelProcessCrc32(md5, chans[i+1]) // crc32(md5(data)
-			crc32 := <-chans[i]
-			crc32Md5 := <-chans[i+1]
+			data := strconv.Itoa(val.(int))
+			crc32Chan := make(chan string, 1)
+			md5Chan := make(chan string, 1)
+
+			go func() {
+				crc32Chan <- DataSignerCrc32(data)
+			}()
+
+			go func() {
+				md5Mux.Lock()
+				md5Data := DataSignerMd5(data)
+				md5Mux.Unlock()
+				md5Chan <- DataSignerCrc32(md5Data)
+			}()
+
+			crc32 := <-crc32Chan
+			crc32Md5 := <-md5Chan
 			result := crc32 + "~" + crc32Md5
 			out <- result
-			i += 2
 		}(val)
 	}
 	wg.Wait()
@@ -68,12 +69,12 @@ func MultiHash(in, out chan interface{}) {
 				workers.Add(1)
 				go func(th int) {
 					defer workers.Done()
-					dataHashes[th] = DataSignerCrc32(fmt.Sprint(th) + data)
+					dataHashes[th] = DataSignerCrc32(strconv.Itoa(th) + data)
 				}(th)
 			}
 			workers.Wait()
 			out <- strings.Join(dataHashes, "")
-		}(fmt.Sprint(data))
+		}(data.(string))
 	}
 
 	wg.Wait()
@@ -82,7 +83,7 @@ func MultiHash(in, out chan interface{}) {
 func CombineResults(in, out chan interface{}) {
 	var result []string
 	for val := range in {
-		result = append(result, fmt.Sprint(val))
+		result = append(result, val.(string))
 	}
 	sort.Strings(result)
 	strRes := strings.Join(result, "_")
